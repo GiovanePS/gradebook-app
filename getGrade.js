37d019790fa103a4fe58dbaf1de19ac4c121eccd
GiovanePS <giovanespace@gmail.com>
2025-07-18 23:41:30 -0300

    Add getGrade: function and tests for it

---
+++ getGrade.js
@@ -0,0 +1,40 @@
+const Errors = require("./errors")
+
+/**
+ * Converts a numeric score to letter score.
+ * The scores and the corresponding letters are:
+ *    - 100:   'A++'
+ *    - 90-99: 'A'
+ *    - 80-89: 'B'
+ *    - 70-79: 'C'
+ *    - 60-69: 'D'
+ *    - 0-59:  'F'
+ *
+ * @param {number} score - the numeric scores (0-100).
+ * @returns {string} grade score.
+ * @example
+ *      getGrade(85) // returns 'B'
+ *      getGrade(95) // returns 'A'
+ *      getGrade(100) // returns 'A++'
+ */
+function getGrade(score) {
+    if (typeof score !== 'number' || score < 0 || score > 100) {
+        throw new Error(Errors.INVALID_INPUT('expected a number between 0 and 100'))
+    }
+
+    if (score === 100) {
+        return 'A++'
+    } else if (score >= 90) {
+        return 'A'
+    } else if (score >= 80) {
+        return 'B'
+    } else if (score >= 70) {
+        return 'C'
+    } else if (score >= 60) {
+        return 'D'
+    } else {
+        return 'F'
+    }
+}
+
+module.exports = { getGrade }
---
+++ tests/getGrade.test.js
@@ -0,0 +1,43 @@
+const Errors = require('../errors.js')
+const { getGrade } = require('../getGrade.js')
+
+describe('getGrade', () => {
+    test('returns the correct letter grade', () => {
+        const grades = [
+            { score: 100, letter: 'A++' },
+            { score: 99, letter: 'A' },
+            { score: 90, letter: 'A' },
+            { score: 89, letter: 'B' },
+            { score: 80, letter: 'B' },
+            { score: 79, letter: 'C' },
+            { score: 70, letter: 'C' },
+            { score: 69, letter: 'D' },
+            { score: 60, letter: 'D' },
+            { score: 59, letter: 'F' },
+            { score: 50, letter: 'F' },
+            { score: 0, letter: 'F' },
+        ]
+
+        grades.forEach(({ score, letter }) => {
+            expect(getGrade(score)).toBe(letter)
+        })
+    })
+
+    test('throws error for a negative score', () => {
+        const negativeScore = -1
+        expect(() => getGrade(negativeScore)).toThrow(Errors.INVALID_INPUT('expected a number between 0 and 100'))
+    })
+
+    test('throws error for a score greater than 100', () => {
+        const scoreGreaterThan100 = 101
+        expect(() => getGrade(scoreGreaterThan100)).toThrow(Errors.INVALID_INPUT('expected a number between 0 and 100'))
+    })
+
+    test.each([
+        'a', {}, [], null, undefined
+    ])('throws error for non-numeric input: %p', (input) => {
+        expect(() => getGrade(input)).toThrow(
+            Errors.INVALID_INPUT('expected a number between 0 and 100')
+        )
+    })
+})
