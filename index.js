dd42cf1c04a6bdc7519e032f5bdec9e9cf3f28dc
GiovanePS <giovanespace@gmail.com>
2025-07-18 23:33:23 -0300

    Add eslint

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
  * @description Just for my testing purposes.
  */
 
-const { getAverage } = require('./getAverage.js');
+const { getAverage } = require('./getAverage.js')
 
 function main() {
-    const scores = [0, 1, NaN];
-    const average = getAverage(scores);
-    console.log(`The average score is: ${average}`);
+    const scores = [0, 1, NaN]
+    const average = getAverage(scores)
+    console.log(`The average score is: ${average}`)
 }
 
 main()
